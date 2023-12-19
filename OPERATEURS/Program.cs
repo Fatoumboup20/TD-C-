e3691c5e800b8ae7087f5d1a47ea6d8db3fdaec8
Fatoumboup20 <mfatoumboup20@gmail.com>
2023-12-19 22:06:47 +0000

    exercice

---
+++ OPERATEURS/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OPERATEURS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("saisir le montant des dommages  ");
+            double dommage = Convert.ToDouble(Console.ReadLine());
+            double franchise = Math.Min(dommage * 0.10, 4000);
+            double rembourse = dommage - franchise;
+            Console.WriteLine("la franchise : " + franchise );
+            Console.WriteLine("le montant remboursé : " + rembourse );
+            Console.ReadKey();
+        }
+    }
+}
